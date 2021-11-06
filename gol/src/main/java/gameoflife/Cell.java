8d819e47c6cd4b854d96bf3951b2bd7b2ea4f415
Leo <leo.ebenezer@hotmail.com>
2021-11-06 11:27:04 +0100

    add rule 3 and more changes

--- gol/src/main/java/gameoflife/Cell.java
+++ gol/src/main/java/gameoflife/Cell.java
@@ -2,8 +2,8 @@ package gameoflife;
 /*
 RULES FOR CONWAY'S GAME OF LIFE
 1. Any live cell with fewer than two live neighbors dies, as if caused by underpopulation.
-2. Any live cell with more than three live neighbors dies, as if by overcrowding.
-3. Any live cell with two or three live neighbors lives on to the next generation.
+2. Any live cell with two or three live neighbors lives on to the next generation.
+3. Any live cell with more than three live neighbors dies, as if by overcrowding.
 4. Any dead cell with exactly three live neighbors becomes a live cell.
 */
 
--- gol/src/test/java/gameoflife/CellTest.java
+++ gol/src/test/java/gameoflife/CellTest.java
@@ -11,54 +11,57 @@ public class CellTest {
   @Test
   public void ShouldDieWithOneNeighbor(){
     Cell cell = new Cell(Cell.CellState.alive);
-
     //Cell.CellState actual = cell.GetNextState(1);
     //i = amount of neighbors
     CellState a = cell.GetNextState(1);
-
     assertEquals(Cell.CellState.dead, a);
   }
   @Test
   public void ShouldDieWithNoNeighbors(){
     Cell cell = new Cell(Cell.CellState.alive);
-
     //i = amount of neighbors
     CellState a = cell.GetNextState(0);
-
     assertEquals(Cell.CellState.dead, a);
   }
   //RULE 1 END
 
-  //RULE 2. Any live cell with more than three live neighbors dies, as if by overcrowding.
+  //RULE 2. Any live cell with two or three live neighbors lives on to the next generation.
   @Test
   public void ShouldLiveWithTwoNeighbors(){
     Cell cell = new Cell(Cell.CellState.alive);
-
     //i = amount of neighbors
     //taking in two neighbors
     CellState a = cell.GetNextState(2);
-
     assertEquals(Cell.CellState.alive, a);
   }
   @Test
-  public void ShouldDieWithThreeNeighbor(){
+  public void ShouldLiveWithThreeNeighbors(){
     Cell cell = new Cell(Cell.CellState.alive);
-
     //i = amount of neighbors
     CellState a = cell.GetNextState(3);
-
     assertEquals(Cell.CellState.alive, a);
   }
+  //RULE 2 END
 
+  //RULE 3. Any live cell with more than three live neighbors dies, as if by overcrowding.
   @Test
-  public void ShouldDieWithFourNeighbor(){
+  public void ShouldDieWithFourNeighbors(){
     Cell cell = new Cell(Cell.CellState.alive);
-
     //i = amount of neighbors
     CellState a = cell.GetNextState(4);
-
     assertEquals(Cell.CellState.dead, a);
   }
-  //RULE 2 END
+  @Test
+  public void ShouldDieWithFiveNeighbors(){
+    Cell cell = new Cell(Cell.CellState.alive);
+    //i = amount of neighbors
+    CellState a = cell.GetNextState(5);
+    assertEquals(Cell.CellState.dead, a);
+  }
+  /*
+    These tests can be repeated to work for any amount
+    of neighbors in a 3x3 grid. Max number is 8 neighbors.
+   */
+  //RULE 3 END
 
 }
