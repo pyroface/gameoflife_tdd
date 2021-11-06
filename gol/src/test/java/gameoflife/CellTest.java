b74d2c3895aa4b4051a049ddc768a1f8f3d6193e
Leo <leo.ebenezer@hotmail.com>
2021-11-06 11:04:21 +0100

    add more tests

--- gol/src/test/java/gameoflife/CellTest.java
+++ gol/src/test/java/gameoflife/CellTest.java
@@ -14,19 +14,51 @@ public class CellTest {
 
     //Cell.CellState actual = cell.GetNextState(1);
     //i = amount of neighbors
-    CellState actual = cell.GetNextState(1);
+    CellState a = cell.GetNextState(1);
 
-    assertEquals(Cell.CellState.dead, actual);
+    assertEquals(Cell.CellState.dead, a);
   }
   @Test
   public void ShouldDieWithNoNeighbors(){
     Cell cell = new Cell(Cell.CellState.alive);
 
     //i = amount of neighbors
-    CellState actual = cell.GetNextState(0);
+    CellState a = cell.GetNextState(0);
 
-    assertEquals(Cell.CellState.dead, actual);
+    assertEquals(Cell.CellState.dead, a);
   }
   //RULE 1 END
+
+  //RULE 2. Any live cell with more than three live neighbors dies, as if by overcrowding.
+  @Test
+  public void ShouldLiveWithTwoNeighbors(){
+    Cell cell = new Cell(Cell.CellState.alive);
+
+    //i = amount of neighbors
+    //taking in two neighbors
+    CellState a = cell.GetNextState(2);
+
+    assertEquals(Cell.CellState.alive, a);
+  }
+  @Test
+  public void ShouldDieWithThreeNeighbor(){
+    Cell cell = new Cell(Cell.CellState.alive);
+
+    //i = amount of neighbors
+    CellState a = cell.GetNextState(3);
+
+    assertEquals(Cell.CellState.alive, a);
+  }
+
+  @Test
+  public void ShouldDieWithFourNeighbor(){
+    Cell cell = new Cell(Cell.CellState.alive);
+
+    //i = amount of neighbors
+    CellState a = cell.GetNextState(4);
+
+    assertEquals(Cell.CellState.dead, a);
+  }
+  //RULE 2 END
 
 }
